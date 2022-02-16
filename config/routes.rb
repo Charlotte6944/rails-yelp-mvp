f2cc3475bd2760b927c4169d7bd8ef03cfe98203
Charlotte MARION <marion.charlotte@orange.fr>
2022-02-16 15:12:41 +0100

    finish exercice

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # un visiteur peut voir la liste de tous les restaurants
+  # get 'restaurants', to: 'restaurants#index'
+
+  # un visiteur peut ajouter un nouveau restau et être redirigé vers la vue show de la page de ce nouveau restau
+  # get 'restaurants/new', to: 'restaurants#new'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # post 'restaurants', to: 'restaurants#create'
+
+  resources :restaurants, only: [:new, :show, :create, :index] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  # resources :reviews, only: [:destroy, :update, :show]
+
+  # get 'restaurants/:restaurant_id/review/new', to: 'reviews#new'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: ["chinese", "italian", "japanese", "french", "belgian"][rand(0..4)]
+  )
+  restaurant.save
+end
