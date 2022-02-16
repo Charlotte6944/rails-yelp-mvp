41b4b49d187dc691afe76c283963d5e80da1b4e6
Charlotte MARION <marion.charlotte@orange.fr>
2022-02-16 11:03:25 +0100

    create models

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  RATINGS = [0, 1, 2, 3, 4, 5]
+  validates :content, presence: true
+  validates :rating, inclusion: { in: RATINGS }, numericality: { only_integer: true }
+
+  belongs_to :restaurant
+end
